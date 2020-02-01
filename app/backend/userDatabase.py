1bbfe08b160c3f1c3e330bb010e9938d9fdf0bfc
Michel Chalfoun <mikachalfoun@gmail.com>
2020-02-01 18:48:28 -0500

    created database

---
+++ app/backend/userDatabase.py
@@ -0,0 +1,16 @@
+import sqlite3
+conn = sqlite3.connect('backend/userDatabase.sqlite')
+cursor = conn.cursor()
+print("Opened database successfully")
+for row in cursor.execute("SELECT courses, credits from courses"):
+    if row[0].startswith('CGS'):
+        print("Course = ", row[0], "Credits = ", row[1])
+conn.commit()
+conn.close()
+
+x = "name"
+
+sqlite_insert_query = """INSERT INTO SqliteDb_developers
+                      (courses, name, email, joining_date, salary) 
+                       VALUES 
+                      (1,  """ + x + """ ,'james@pynative.com','2019-03-17',8000)"""
