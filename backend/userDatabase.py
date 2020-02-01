e7c1c589fc104a09d886ed4c0760915dcf038b6f
Masen <masenbeliveau@gmail.com>
2020-02-01 17:10:11 -0500

    Tested different database values

--- backend/userDatabase.py
+++ backend/userDatabase.py
@@ -3,7 +3,7 @@ conn = sqlite3.connect('userDatabase.sqlite')
 cursor = conn.cursor()
 print("Opened database successfully")
 for row in cursor.execute("SELECT courses, credits from courses"):
-    if row[0].startswith('COP'):
+    if row[0].startswith('CGS'):
         print("Course = ", row[0], "Credits = ", row[1])
 conn.commit()
 conn.close()
