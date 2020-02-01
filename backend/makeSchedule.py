642e1f750c3c5893cbee7c5deaa9ed1196a139f2
Stefan Hengartner <stefanhengartner@ufl.edu>
2020-02-01 18:39:53 -0500

    updated makeSchedule.py

--- backend/makeSchedule.py
+++ backend/makeSchedule.py
@@ -1,20 +1,45 @@
 import student
 
+
 def makeSchedule(s=student.Student):
-    print(
-        "Rank the following 1-3, with 3 being most important to you: Course Difficulty, Past Success in Subject, "
-        "Taking PreReqs for Other Courses -\n")
+    choice = int(input("Would you like to create a schedule for you with default settings or would you like to input "
+                       "manual settings?"))
+    if choice == "default":
 
-    coursePriorityIndex = int(input("Course Difficulty: "))
-    pastSuccessIndex = int(input("Past Success in Subject: "))
-    bottleneckIndex = int(input("PreReqs for Other Courses: "))
+        print(
+            "Rank the following 3 options on a 1-5 scale, with 1 being 'I strongly disagree' and 5 being 'I strongly "
+            "agree': 'I would like to take a rigorous courseload'; 'I would like to take courses in subjects I "
+            "have had success with in the past'; 'I would like to take courses that are prerequisites for many other "
+            "courses' -\n")
 
-    print("\nWhat credit range do would you like for your semester? ")
+        coursePriorityIndex = int(input("'I would like to take a rigorous courseload': "))
+        pastSuccessIndex = int(input("'I would like to take courses in subjects I have had success with in the past': "))
+        bottleneckIndex = int(input("'I would like to take courses that are prerequisites for many other courses': "))
 
-    creditNumLow = int(input("Lower end: "))
-    creditNumHigh = int(input("Upper end: "))
+    elif choice == "manual":
+        totalCreditsTaken = 0
+        for c in s.classes:
+            totalCreditsTaken += c.credits
+        if totalCreditsTaken <= 30:
+            coursePriorityIndex = 2
+            pastSuccessIndex = 1
+            bottleneckIndex = 5
+        elif totalCreditsTaken <= 60:
+            coursePriorityIndex = 3
+            pastSuccessIndex = 3
+            bottleneckIndex = 4
+        elif totalCreditsTaken <= 90:
+            coursePriorityIndex = 5
+            pastSuccessIndex = 4
+            bottleneckIndex = 3
+        else:
+            coursePriorityIndex = 4
+            pastSuccessIndex = 5
+            bottleneckIndex = 2
 
-    semesterDifficulty = 0.0
+
+    creditNumHigh = int(input("\nWhat is the maximum number of credits you would like to take your next semester? "))
+
     curCreditNum = 0
 
     courseList = []
@@ -22,7 +47,7 @@ def makeSchedule(s=student.Student):
 
     for c in courseList:
         courseRanking = coursePriorityIndex * c.difficulty + pastSuccessIndex * s.getAvg(c.category) + bottleneckIndex
-            # * number of classes in CourseList this course is a prereq for
+        # * number of classes in CourseList this course is a prereq for
         c.curRank = courseRanking
 
     courseList.sort(key=lambda x: x.curRank, reverse=True)
@@ -31,19 +56,22 @@ def makeSchedule(s=student.Student):
     schedule = []
 
     while curCreditNum < creditNumHigh and i < len(courseList):
-        if curCreditNum + courseList[i].credits > creditNumHigh:
+        if curCreditNum == creditNumHigh:
+            break
+        elif curCreditNum + courseList[i].credits > creditNumHigh:
             i += 1
             continue
         else:
             schedule.append(courseList[i])
             curCreditNum += courseList[i].credits
-            updateCourseRanking(courseList[i], courseList)
-            i += 1
+            course = courseList[i].pop
+            updateCourseRanking(course, courseList)
     return schedule
 
-makeSchedule(student.myStudent_01)
 
 def updateCourseRanking(course, courseList):
     for c in courseList:
         if course.category == c.category:
             c.curRank -= course.difficulty
+        if course.difficulty > c.difficulty:
+            c.curRank -= 2 * (c.difficulty - course.difficulty)
