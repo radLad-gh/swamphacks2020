c32ee86a32ac38687ec848dfdfd7be85faead1b5
Mahan Mahtabfar <mmahtabfar1@gmail.com>
2020-02-01 10:23:55 -0500

    added redirection to login page

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,17 @@
 from flask import render_template
+from flask import redirect
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
 @app.route("/index")
 
 def index():
-    user = {"username": "Mahan"}
-    return render_template("index.html", name="home", user=user)
+    return redirect("/login")
+
+@app.route("/login")
+
+def login():
+    form = LoginForm()
+    return render_template("login.html", title = "Sign in", form = form)
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-class Config(self):
+class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
