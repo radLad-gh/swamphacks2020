484c5515e5a7e292a3536e1a59cb3d991e2e295f
Mahan Mahtabfar <mmahtabfar1@gmail.com>
2020-02-01 20:02:52 -0500

    deleted backend folder

--- backend/userDatabase.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('userDatabase.sqlite')
-cursor = conn.cursor()
-print("Opened database successfully")
-for row in cursor.execute("SELECT courses, credits, prereqs, coreqs from courses"):
-    if row[0].startswith('CGS'):
-        print("Course = ", row[0], "Credits = ", row[1], "Prereqs = ", row[2], "Coreqs = ", row[3])
-conn.commit()
-conn.close()
