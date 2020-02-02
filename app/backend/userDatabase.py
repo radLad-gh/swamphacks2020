51d209f748838c999012cfc17a89ca96054cbdd5
Masen <masenbeliveau@gmail.com>
2020-02-01 19:19:10 -0500

    Updated

--- app/backend/userDatabase.py
+++ app/backend/userDatabase.py
@@ -1,5 +1,4 @@
 import sqlite3
-<<<<<<< HEAD
 conn = sqlite3.connect('backend/userDatabase.sqlite')
 cursor = conn.cursor()
 print("Opened database successfully")
@@ -15,7 +14,6 @@ sqlite_insert_query = """INSERT INTO SqliteDb_developers
                       (courses, name, email, joining_date, salary) 
                        VALUES 
                       (1,  """ + x + """ ,'james@pynative.com','2019-03-17',8000)"""
-=======
 conn = sqlite3.connect('userDatabase.sqlite')
 cursor = conn.cursor()
 print("Opened database successfully")
@@ -24,4 +22,3 @@ for row in cursor.execute("SELECT courses, credits from courses"):
         print("Course = ", row[0], "Credits = ", row[1])
 conn.commit()
 conn.close()
->>>>>>> edd672d00aa643d527492e59e1628e6b2de7bd2f
